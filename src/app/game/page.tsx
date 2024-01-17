8715003545381905d84be09a2164b7e5ec519278
stanislav.semeniukk <stanislav.semeniukk@gmail.com>
2024-01-17 22:16:33 +0200

    fix: rewrited start game form using hook form

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { Box, Button, FormControl, Select, MenuItem, InputLabel } from '@mui/material';
+import { Box, Button, FormControl, Select, MenuItem, InputLabel, Typography } from '@mui/material';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import { useRouter } from 'next/navigation';
 
@@ -10,17 +10,28 @@ import { useAuthContext } from '@/context/AuthContext';
 import { useNotificationContext } from '@/context/NotificationContext';
 import { createGame } from '@/firebase/db';
 import { rewriteCategory, capitalizeString } from '@/helpers/strings';
+import { useForm, Controller } from 'react-hook-form';
+import Loading from '@/components/Loading';
+
+type FormSelects = {
+  difficulty: string;
+  category: string;
+};
 
 export default function Game() {
   const [difficulties, setDifficulties] = useState<string[]>([]);
   const [categories, setCategories] = useState<string[]>([]);
 
-  const [chosenDifficulty, setDifficulty] = useState('');
-  const [chosenCategory, setCategory] = useState('');
-
   const router = useRouter();
   const { user } = useAuthContext();
   const { enableNotification } = useNotificationContext();
+
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors, isSubmitting },
+  } = useForm<FormSelects>();
 
   useEffect(() => {
     if (user === null) router.push('/');
@@ -32,18 +43,16 @@ export default function Game() {
       const difficultiesData = await getDifficultiesForAGame();
       setCategories(categoriesData);
       setDifficulties(difficultiesData);
-      setCategory(categoriesData[0]);
-      setDifficulty(difficultiesData[0]);
     }
     fetchData();
   }, []);
 
-  async function startNewGame() {
+  async function startNewGame(data: FormSelects) {
     if (user) {
       const { result: gameInfo, error } = await createGame(
         user.uid,
-        chosenCategory,
-        chosenDifficulty,
+        data.category,
+        data.difficulty,
       );
       if (!error && gameInfo) router.push(`/game/${gameInfo?.id}`);
       else enableNotification({ type: 'error', message: error || 'Error' });
@@ -51,47 +60,77 @@ export default function Game() {
   }
 
   return (
-    <Box display="flex" flexDirection="column" gap="24px" minWidth="400px" alignItems="center">
+    <Box
+      onSubmit={handleSubmit(startNewGame)}
+      display="flex"
+      component="form"
+      flexDirection="column"
+      gap="24px"
+      minWidth="400px"
+      alignItems="center">
       <Button
+        disabled={isSubmitting}
+        type="submit"
         sx={{ width: 'fit-content' }}
-        onClick={() => startNewGame()}
         variant="contained"
         color="secondary">
         <PlayArrowIcon />
       </Button>
       <FormControl fullWidth>
         <InputLabel id="difficulty-label">Difficulty</InputLabel>
-        <Select
-          color="secondary"
-          variant="filled"
-          labelId="difficulty-label"
-          id="demo-simple-select"
-          value={chosenDifficulty || ''}
-          label="Difficulty"
-          onChange={(e) => setDifficulty(e.target.value)}>
-          {difficulties.map((difficulty, index) => (
-            <MenuItem key={index} value={difficulty}>
-              {capitalizeString(difficulty)}
-            </MenuItem>
-          ))}
-        </Select>
+        <Controller
+          name="difficulty"
+          defaultValue=""
+          control={control}
+          rules={{ required: 'Difficulty is required' }}
+          render={({ field }) => (
+            <Select
+              {...field}
+              color="secondary"
+              variant="filled"
+              labelId="difficulty-label"
+              label="Difficulty">
+              {difficulties.map((difficulty, index) => (
+                <MenuItem key={index} value={difficulty}>
+                  {capitalizeString(difficulty)}
+                </MenuItem>
+              ))}
+            </Select>
+          )}
+        />
+        {errors.difficulty && (
+          <Typography color="red" margin="0 4px" fontSize="12px">
+            {errors.difficulty.message}
+          </Typography>
+        )}
       </FormControl>
       <FormControl fullWidth>
         <InputLabel id="category-label">Category</InputLabel>
-        <Select
-          color="secondary"
-          variant="filled"
-          labelId="category-label"
-          id="demo-simple-select"
-          value={chosenCategory || ''}
-          label="Category"
-          onChange={(e) => setCategory(e.target.value)}>
-          {categories.map((category, index) => (
-            <MenuItem key={index} value={category}>
-              {rewriteCategory(category)}
-            </MenuItem>
-          ))}
-        </Select>
+        <Controller
+          name="category"
+          defaultValue=""
+          control={control}
+          rules={{ required: 'Category is required' }}
+          render={({ field }) => (
+            <Select
+              {...field}
+              color="secondary"
+              variant="filled"
+              labelId="category-label"
+              label="Category">
+              {categories.map((category, index) => (
+                <MenuItem key={index} value={category}>
+                  {rewriteCategory(category)}
+                </MenuItem>
+              ))}
+            </Select>
+          )}
+        />
+        {errors.category && (
+          <Typography color="red" margin="0 4px" fontSize="12px">
+            {errors.category.message}
+          </Typography>
+        )}
       </FormControl>
     </Box>
   );
